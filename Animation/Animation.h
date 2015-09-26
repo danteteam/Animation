594b28d831507d421b57810b82a6f000bd9f4bfe
Ivan Brazhnikov <samsungpc239@gmail.com>
2015-09-26 18:34:38 +0300

    Initial Commit

---
+++ Animation/Animation.h
@@ -0,0 +1,19 @@
+//
+//  Animation.h
+//  Animation
+//
+//  Created by Ivan Brazhnikov on 26/09/15.
+//  Copyright © 2015 dantelab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Animation.
+FOUNDATION_EXPORT double AnimationVersionNumber;
+
+//! Project version string for Animation.
+FOUNDATION_EXPORT const unsigned char AnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Animation/PublicHeader.h>
+
+
